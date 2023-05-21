001fe83f05542da2d4b4bb4234135b4b6d5cea76
Muhammad Faizan <128040410+MuhammadFaizan5478@users.noreply.github.com>
2023-05-21 23:51:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    color: Colors.grey,
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+    routes: {
+      '/login': (context) => const loginpage(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
 
-  // This widget is the root of your application.
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), navigateToNoteList);
+  }
+
+  void navigateToNoteList() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => loginpage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: loginpage()
+    return Scaffold(
+      backgroundColor: Colors.red[200],
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome to JMMM Shop',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              const SizedBox(height: 10.0),
+              const Text(
+                'Press on Login to explore our shop',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.black),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: navigateToNoteList,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
+                  backgroundColor: Colors.red,
+                ),
+                child: const Text(
+                  'login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
